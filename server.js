9ea6f8a89a364d1e51f91d2f92e24f08383d7410
Dave Wai <tina.dave.wai@gmail.com>
2018-05-30 21:48:52 +1000

    Initial Commit

---
+++ server.js
@@ -0,0 +1,50 @@
+// Return a function
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+// set http route handler
+app.get('/', (req,res) => {
+  //res.send('<h1>Hello Express</h1>');
+  // res.send({
+  //   name: 'Andrew',
+  //   likes: [
+  //     'Biking',
+  //     'Basketball'
+  //   ]
+  // })
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    currentYear: new Date().getFullYear(),
+    welcomeMessage: 'Welcome Dave!!'
+  })
+});
+
+app.get('/about', (req, res) => {
+  //res.send('About Page');
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  });
+});
+
+
+
+// /bad - request failed - send back
+//json with errorMessage
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request'
+  });
+});
+
+
+
+// bind app to port on the machine
+app.listen(3000, () => {
+  console.log('Server is up on port3000');
+});
